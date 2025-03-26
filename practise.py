c0691510427dbf118b4302029fab795773ed386f
Barnice-Nyawira <barnicenyawira@gmail.com>
2025-03-26 11:35:01 -0800

    merge remote and local

--- practise.py
+++
@@ -1,6 +0,0 @@
-print("hello world")
-print("barnice nyawira")
-print("i love coding")
-print("moringa is cool")
-print("its a cold wednesday morning")
->>>>>>> 4c512697f417154d1f31a110e7fb6a5f6746f841
