15c49cf122bc37f7d89da9802d25e95142c4f204
Barnice-Nyawira <barnicenyawira@gmail.com>
2025-03-26 00:47:10 -0800

    Update practise.py

--- practise.py
+++ practise.py
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
-=======
 print("hello world")
 print("barnice nyawira")
 print("i love coding")
 print("moringa is cool")
 print("its a cold wednesday morning")
 print("wrc 2025")
+print("python is cool")
